46cfb578f6350ef56ac444c049d7d071c7405ddf
jack upt <jhonbahama@gmail.com>
2020-02-10 20:34:11 -0800

    nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SearchName from "./SearchName";
+import SearchDOB from "./SearchDOB";
+
+import "Nav.css";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+<button
+className="navbar-toggler"
+type="button"
+data-toggle="collapse"
+data-target="#navbarNav"
+aria-controls="navbarNav"
+aria-expanded="false"
+aria-label="Toggle navigation"
+>
+    <span className="navbar-toggler-icon" />
+</button>
+<div className="collapse navbar-collapse row" id="navbarNav">
+    <div className="search-area col-4">
+        <SearchName />
+        <SearchDOB />
+    </div>
+</div>
+</nav>
+    );
+}
+export default Nav;
